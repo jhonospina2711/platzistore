61dccd4ac1730b16ec4171b5e3c06a58b3a9f5f8
JHON GABRIEL OSPINA OROZCO <jhonospina2711@gmail.com>
2024-05-10 00:11:50 -0500

    Se conecta con la api de platzi para traer los articulos

---
+++ src/app/domains/info/pages/about/about.component.ts
@@ -0,0 +1,30 @@
+import { Component, signal, Input } from '@angular/core';
+
+import { CounterComponent } from './../../../shared/components/counter/counter.component'
+import { WaveAudioComponent } from './../../../info/components/wave-audio/wave-audio.component'
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [CommonModule, CounterComponent, WaveAudioComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+  duration = signal(1000);
+  message = signal('Hola');
+
+  changeDuration(event: Event){
+    const input = event.target as HTMLInputElement;
+    this.duration.set(input.valueAsNumber)
+  }
+
+  changeMessage(event: Event){
+    const input = event.target as HTMLInputElement;
+    this.message.set(input.value)
+  }
+
+
+
+}
